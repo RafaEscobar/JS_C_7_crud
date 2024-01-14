525186816ff0c468c25bc0e801673d8a2e4e3807
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-01-14 14:27:55 -0600

    Construyendo modulo de peticion de usuarios al API

--- src/users/modules/load-users-by-page.js
+++ src/users/modules/load-users-by-page.js
@@ -1,3 +1,11 @@
-export const loadUsersByPage = () => {
-    
+/**
+ * Function to load the page of users
+ * @param {integer} page 
+ */
+export const loadUsersByPage = async(page = 1) => {
+    const url = `${import.meta.env.VITE_URL_BASE}/users?_page=${page}`
+    const res = await fetch(url);
+    const resBody = await res.json();
+
+    console.log(resBody);
 }
